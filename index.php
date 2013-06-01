1577534b9d19d02daa8711d2867f3df322e713e7
Damien Costa <te-user@hotmail.co.uk>
2013-06-02 00:20:58 +0200

    Add Cache.

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+	session_start();
 	require_once("functions.v4r.php");
-	echo $Server->HistoryVote;
+	echo $Server->Name;
+	echo $Server->Position;
+	echo $Server->Vote;
+	echo $Server->ClicOut;
+	// ...
 ?>
